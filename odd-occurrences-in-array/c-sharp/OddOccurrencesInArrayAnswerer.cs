f26ae9385f6f070101457e32b49e320b6522ea91
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-03-27 02:32:29 +0300

    I answered Odd Occurrences In Array algorithm with c#

---
+++ odd-occurrences-in-array/c-sharp/OddOccurrencesInArrayAnswerer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OddOccurrencesInArray
+{
+    class OddOccurrencesInArrayAnswerer
+    {
+        public static int Solution(int[] A)
+        {
+            var orderedA = A.OrderBy(i => i).ToList();
+
+            int i;
+
+            for (i = 0; i < orderedA.Count() - 1 && orderedA[i] == orderedA[i + 1]; i += 2) ;
+
+            return orderedA[i];
+        }
+    }
+
+}
---
+++ odd-occurrences-in-array/c-sharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OddOccurrencesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 1, 3, 5, 7, 9, 13, 3, 1, 5, 7, 9 };
+            Console.WriteLine(OddOccurrencesInArrayAnswerer.Solution(numbers));
+        }
+    }
+
+}
