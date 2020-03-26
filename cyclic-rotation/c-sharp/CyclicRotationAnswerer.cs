18af8b9b6f1ec522235fc10aaa760ec2a23d57a3
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-03-26 18:14:22 +0300

    I answered Cyclic Rotation algorithm with c#

---
+++ cyclic-rotation/c-sharp/CyclicRotationAnswerer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyclicRotation
+{
+    class CyclicRotationAnswerer
+    {
+        public static int[] Solution(int[] A, int K)
+        {
+            if (K == 0)
+            {
+                return A;
+            }
+
+            int[] newNumbersArray = new int[A.Length];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                int value = A[i];
+                int indexOfNewArray = (i + K) % A.Length;
+
+                newNumbersArray[indexOfNewArray] = value;
+            }
+
+            return newNumbersArray;
+        }
+    }
+}
---
+++ cyclic-rotation/c-sharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CyclicRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = new int[] { 1, 2, 3, 4 };
+            int[] secondArray = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            int[] thirdArray = new int[] { 0, 0 };
+
+            List<int[]> arrays = new List<int[]>
+            {
+                firstArray,secondArray,thirdArray
+            };
+
+            for (int i = 0; i < arrays.Count; i++)
+            {
+                var array = arrays[i];
+                var newArray = CyclicRotationAnswerer.Solution(array, 6);
+
+                for (int j = 0; j < newArray.Length; j++)
+                {
+                    Console.Write($"{newArray[j]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
