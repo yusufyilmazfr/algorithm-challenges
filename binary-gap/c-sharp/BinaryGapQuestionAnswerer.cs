c1c5df071dd1a710aea13e58e0c078590af02767
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-03-26 17:25:16 +0300

    I answered Binary Gap algorithm with c#

---
+++ binary-gap/c-sharp/BinaryGapQuestionAnswerer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryGap
+{
+    class BinaryGapQuestionAnswerer
+    {
+        public static int Solution(int n)
+        {
+            var binaries = Convert.ToString(n, 2).ToCharArray();
+
+            if (binaries.Contains('0') == false)
+            {
+                return 0;
+            }
+
+            int maxLengthOfGap = 0;
+            int counter = 0;
+            int indexOfBinaries = 0;
+
+            while (indexOfBinaries < binaries.Length)
+            {
+                char value = binaries[indexOfBinaries];
+                indexOfBinaries++;
+
+                if (value == '1')
+                {
+                    if (counter > maxLengthOfGap)
+                    {
+                        maxLengthOfGap = counter;
+                    }
+
+                    counter = 0;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+
+            return maxLengthOfGap;
+        }
+    }
+
+}
---
+++ binary-gap/c-sharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace BinaryGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = new int[] { 9, 529, 20, 15, 32, 51712, 3, 6291457, 561892, 74901729, 805306373, 1073741825 };
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                int value = values[i];
+                int maxLengthOfGap = BinaryGapQuestionAnswerer.Solution(value);
+
+                Console.WriteLine($"{ Convert.ToString(value, 2)} --- > {maxLengthOfGap}");
+            }
+        }
+    }
+}
