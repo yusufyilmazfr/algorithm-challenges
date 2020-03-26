70273b4a19a763521055d461eaf397503a680183
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-03-27 02:52:39 +0300

    I answered Frog Jump algorithm with c#

---
+++ frog-jump/FrogJmpAnswerer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrogJmp
+{
+    class FrogJmpAnswerer
+    {
+        public static int Solution(int X, int Y, int D)
+        {
+            int gap = Y - X;
+
+            bool hasCarry = false;
+
+            if (gap % D != 0)
+            {
+                hasCarry = true;
+            }
+
+            return hasCarry ? gap / D + 1 : gap / D;
+        }
+    }
+
+}
---
+++ frog-jump/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FrogJmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FrogJmpAnswerer.Solution(10, 85, 30));
+        }
+    }
+}
